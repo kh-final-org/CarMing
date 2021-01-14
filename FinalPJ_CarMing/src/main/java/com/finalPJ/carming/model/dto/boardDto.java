c6c7c2ba89c8e55ea22cfd03d69d6be7b5ddc246
hwangjm <ghkdwjdals33@naver.com>
2021-01-15 01:21:04 +0900

    210114_Board 게시글 댓글 수 컬럼 추가

--- FinalPJ_CarMing/src/main/java/com/finalPJ/carming/model/dto/boardDto.java
+++ FinalPJ_CarMing/src/main/java/com/finalPJ/carming/model/dto/boardDto.java
@@ -9,7 +9,8 @@ public class boardDto {
 	private String brdwriter;	//게시글 작성자
 	private String brdfile;		//파일명
 	private String brdcontent;	//게시글 내용
-	private int brdcount;		//게시글 조회수
+	private int brdcount;		//게시글 조회 수
+	private int brdcomcount;	//게시글 댓글 수 
 	private Date brddate;		//게시글 작성 날짜
 	private String brdprivate;	//나만보기 체크 여부
 	
@@ -17,8 +18,9 @@ public class boardDto {
 		super();
 	}
 
+	
 	public boardDto(int brdno, int bcategoryno, String brdwriter, String brdfile, String brdcontent, int brdcount,
-			Date brddate, String brdprivate) {
+			int brdcomcount, Date brddate, String brdprivate) {
 		super();
 		this.brdno = brdno;
 		this.bcategoryno = bcategoryno;
@@ -26,58 +28,84 @@ public class boardDto {
 		this.brdfile = brdfile;
 		this.brdcontent = brdcontent;
 		this.brdcount = brdcount;
+		this.brdcomcount = brdcomcount;
 		this.brddate = brddate;
 		this.brdprivate = brdprivate;
 	}
 
+
 	public int getBrdno() {
 		return brdno;
 	}
+
 	public void setBrdno(int brdno) {
 		this.brdno = brdno;
 	}
+
 	public int getBcategoryno() {
 		return bcategoryno;
 	}
+
 	public void setBcategoryno(int bcategoryno) {
 		this.bcategoryno = bcategoryno;
 	}
+
 	public String getBrdwriter() {
 		return brdwriter;
 	}
+
 	public void setBrdwriter(String brdwriter) {
 		this.brdwriter = brdwriter;
 	}
+
 	public String getBrdfile() {
 		return brdfile;
 	}
+
 	public void setBrdfile(String brdfile) {
 		this.brdfile = brdfile;
 	}
+
 	public String getBrdcontent() {
 		return brdcontent;
 	}
+
 	public void setBrdcontent(String brdcontent) {
 		this.brdcontent = brdcontent;
 	}
+
 	public int getBrdcount() {
 		return brdcount;
 	}
+
 	public void setBrdcount(int brdcount) {
 		this.brdcount = brdcount;
 	}
+
+	public int getBrdcomcount() {
+		return brdcomcount;
+	}
+
+	public void setBrdcomcount(int brdcomcount) {
+		this.brdcomcount = brdcomcount;
+	}
+
 	public Date getBrddate() {
 		return brddate;
 	}
+
 	public void setBrddate(Date brddate) {
 		this.brddate = brddate;
 	}
+
 	public String getBrdprivate() {
 		return brdprivate;
 	}
+
 	public void setBrdprivate(String brdprivate) {
 		this.brdprivate = brdprivate;
 	}
+
 
 
 	
