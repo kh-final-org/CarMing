e801c398a8a162394a3e7197d7b007f4a384d353
changonna <tyu4778@gmail.com>
2021-01-19 19:55:34 +0900

    cg_MemberDto stashes
    MemberDto 쓰던거 stashes 다시 추가

--- FinalPJ_CarMing/src/main/java/com/finalPJ/carming/model/dto/MemberDto.java
+++ FinalPJ_CarMing/src/main/java/com/finalPJ/carming/model/dto/MemberDto.java
@@ -1,5 +1,20 @@
 package com.finalPJ.carming.model.dto;
 
-public class MemberDto {
+import java.util.Date;
 
+public class MemberDto {
+	private int memno;
+	private String memnick;
+	private String memid;
+	private String mempw;
+	private String memname;
+	private String memzip;
+	private String memaddr;
+	private String memaddr2;
+	private String memphone;
+	private Date membirth;
+	private int memgender;
+	private String memchk;
+	private int memcode;
+	private String memfile;
 }
