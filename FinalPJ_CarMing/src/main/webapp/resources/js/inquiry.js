36b393cf2b6fafc506a6b683bbe760eba02bfcd1
Yesul <yesullee93@gmail.com>
2021-02-10 22:50:37 +0900

    예슬
    inquiry 자바스크립트

---
+++ FinalPJ_CarMing/src/main/webapp/resources/js/inquiry.js
@@ -0,0 +1,99 @@
+	var sel_file;
+	$(document).ready(function() {
+		$("#input_img").on("change", handleImgFileSelect);
+	});
+
+	function handleImgFileSelect(e) {
+		$("#img").empty(); //empty, remove 구분! remove는 태그 자체를 지운다 오키?
+		var files = e.target.files;
+		var filesArr = Array.prototype.slice.call(files);
+
+		filesArr.forEach(function(f) {
+			if (!f.type.match("image.*")) {
+				alert("확장자는 이미지 확장자만 가능합니다.");
+				return;
+			}
+			sel_file = f;
+
+			var reader = new FileReader();
+			reader.onload = function(e) {
+				$("#img").attr("src", e.target.result);
+				$("#img").attr("width", "90%");
+				$("#img").attr("height", "440px");
+			}
+			reader.readAsDataURL(f);
+		})
+	}
+
+
+$(document).ready(function(){
+	var category = $('select[name=categoryNo]');
+	var content = $('textarea[name=inquiryContent]');	
+	var img = $('input[type=file]');
+		
+		if(category.val() == null){
+				$('#emptycategory').show();
+			}
+		
+		category.change(function(){
+				$('#emptycategory').hide();
+				});
+		
+		if(content.text().trim() == ""){
+			$('#emptycontent').show();
+		}
+		if(content.text().trim() != null){
+			content.keyup(function(){
+			$('#emptycontent').hide();
+			console.log(content.text())
+			});
+		}
+		
+		if(img.val() == ""){
+			$('#emptyfile').show();
+		}
+		
+		img.change(function(){
+			$('#emptyfile').hide();
+			
+		var filename = img.val().split('\\').pop();
+        $('#filename').text(filename);
+			
+		});
+		
+		$( "#button-boardupload" ).click(function() {
+		var category = $('select[name=categoryNo]').val();
+		var content = $('textarea[name=inquiryContent]');
+		var img = $('input[type=file]').val();
+		
+		
+				/*if(category == null ||
+				content.text().trim() == "" 	||
+				img == "" ){
+			
+					alert("모든 항목을 작성해 주세요");
+					return;
+					
+					
+				}*/
+				
+				if(category != null &&
+				content.text() != "" 	&&
+				img != null  )	{
+					alert("문의주셔서 감사합니다.");
+					$("#target").submit();
+					alert(category+"/"+content+"/"+img);
+				}
+					
+					
+				
+				
+		});
+
+
+});	
+
+
+
+	
+
